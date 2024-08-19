7a4f83fdc8ab0f1c4674925229122705c1faad20
Asahi-000 <160554947+Asahi-000@users.noreply.github.com>
2024-08-18 20:01:05 -0700

    Add files via upload

---
+++ ACTIVITY_FELISELDAKIMBERLY/variables_feliseldakimberly.php
@@ -0,0 +1,10 @@
+<?php
+
+    $name = "Kimberly Feliselda";
+    $age = 20;
+    $favorite_color = "blue";
+
+    echo "My name is $name, I am $age years old, and my favorite color is $favorite_color.";
+
+
+?>
